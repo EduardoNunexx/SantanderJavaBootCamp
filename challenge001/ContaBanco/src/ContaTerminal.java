4e36a30f3e4c0212ce67170a8f888f96ec6d0a36
EduardoNunexx <eduardonunes@alu.ufc.br>
2024-06-19 17:19:41 -0300

    fc

---
+++ challenge001/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Por favor, digite o número!");
+        numero=sc.nextInt();
+        sc.nextLine();
+        System.out.println("Por favor, digite a agencia!");
+        agencia=sc.nextLine();
+        System.out.println("Por favor, digite o nome do cliente!");
+        nomeCliente= sc.nextLine();
+        System.out.println("Por favor, digite o saldo");
+        saldo = sc.nextDouble();
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, su agênca é "+agencia+",conta "+numero+" e seu saldo "+saldo+" já está disponível para saque !");
+
+    }
+}
