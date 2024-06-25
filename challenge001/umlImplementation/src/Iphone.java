a602efb75bd6af2ab189c0139e7d77efc2d24203
EduardoNunexx <eduardonunes@alu.ufc.br>
2024-06-25 16:20:18 -0300

    iphone Implementation challenge 003

---
+++ challenge001/umlImplementation/src/Iphone.java
@@ -0,0 +1,47 @@
+public class Iphone implements NavegadorInternet,ReprodutorMusical,AparelhoTelefonico{
+    @Override
+    public void ligar() {
+        System.out.println("Calling");
+    }
+
+    @java.lang.Override
+    public void atender() {
+
+    }
+
+    @java.lang.Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @java.lang.Override
+    public void ExbirPagina(String url) {
+
+    }
+
+    @java.lang.Override
+    public void adcionarNovaAba() {
+
+    }
+
+    @java.lang.Override
+    public void atualizarPagina() {
+
+    }
+
+    @java.lang.Override
+    public void tocar() {
+
+    }
+
+    @java.lang.Override
+    public void pausar() {
+
+    }
+
+    @java.lang.Override
+    public void selecionarMusica(String musica) {
+
+    }
+
+}
---
+++ challenge001/umlImplementation/src/NavegadorInternet.java
@@ -0,0 +1,5 @@
+public interface NavegadorInternet {
+    public void ExbirPagina(String url);
+    public void adcionarNovaAba();
+    public void atualizarPagina();
+}
